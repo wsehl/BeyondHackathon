4d77738afe6c819c850b04ffc35ee2f3e73df435
wsehl <widesehl@gmail.com>
2020-09-29 16:04:40 +0600

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>My custom Bulma website</title>
+    <title>Greenmeetings</title>
     <link rel="stylesheet" href="css/mystyles.css">
     <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
 </head>
@@ -30,20 +30,20 @@
                     <div id="navbarMenuHeroB" class="navbar-menu">
                         <div class="navbar-end">
                             <a class="navbar-item is-active">
-                                Home
+                                Главная
                             </a>
                             <a class="navbar-item">
-                                Examples
+                                Наши проекты
                             </a>
                             <a class="navbar-item">
-                                Documentation
+                                О нас
                             </a>
                             <span class="navbar-item">
                                 <a class="button is-info is-inverted">
                                     <span class="icon">
-                                        <i class="fab fa-github"></i>
+                                        <i class="far fa-user"></i>
                                     </span>
-                                    <span>Download</span>
+                                    <span>Войти</span>
                                 </a>
                             </span>
                         </div>
