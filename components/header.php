c15fc9c95df5abd09e4481ebd2da19116e2a7818
wsehl <widesehl@gmail.com>
2020-09-30 13:24:05 +0600

    css

--- components/header.php
+++ components/header.php
@@ -10,6 +10,7 @@
     <title>Greenmeetings</title>
     <link rel="stylesheet" href="css/mystyles.css">
     <link rel="stylesheet" href="css/bulma-checkradio.min.css">
+    <link rel="stylesheet" href="css/modal-fx.min.css">
     <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
