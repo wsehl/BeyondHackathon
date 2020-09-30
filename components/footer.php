c6395ae8b905499905af0b8ab4a14c7ef555ac79
wsehl <widesehl@gmail.com>
2020-09-30 18:46:41 +0600

    login

--- components/footer.php
+++ components/footer.php
@@ -2,6 +2,7 @@
 ?>
 
 <script src="js/modal-fx.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 
 <script>
     document.addEventListener('DOMContentLoaded', () => {
--- login.php
+++ login.php
@@ -56,85 +56,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     mysqli_close($link);
 }
 ?>
-<!DOCTYPE html>
-<html>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Авторизация</title>
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css">
-    <link rel="stylesheet" href="css/style.css" />
-</head>
 
-<body>
-    <nav id="navbar" class="bd-navbar navbar is-spaced is-fixed-top">
-        <div class="container">
-            <div class="navbar-brand">
-                <a class="navbar-item" href="index.php"><img src="images/logo.png" alt="Restaurant X" width="112" height="28"></a>
-                <div id="navbarBurger" class="navbar-burger burger" data-target="navMenuDocumentation">
-                    <span></span><span></span><span></span>
-                </div>
-            </div>
-            <div id="navMenuDocumentation" class="navbar-menu">
-                <div class="navbar-end">
-                    <a class="navbar-item" href="about.php"><span>О компании</span></a>
-                    <a class="navbar-item" href="contact.php"><span>Контакты</span></a>
-                    <a class="navbar-item" href="cart.php"><span>Корзина&nbsp;</span><span class="tag is-success is-small" id="all">0</span></a>
-                </div>
-                <div class="navbar-end">
-                    <div class="navbar-item">
-                        <div class="buttons">
-                            <?php
-                            if ($log == 0) {
-                            ?>
-                                <a href="login.php" class="button is-small">Войти</a>
-                                <a href="register.php" class="button is-success is-small"><strong>Регистрация</strong></a>
-                            <?php
-                            } else if ($log == 1) {
-                            ?>
-                                <a href="welcome.php" class="button is-small">Профиль</a>
-                                <a href="logout.php" class="button is-danger is-small">Выйти</a>
-                            <?php
-                            }
-                            ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </nav>
-    <div class="container">
-        <section class="hero is-large">
-            <div class="hero-body">
-                <div class="columns">
-                    <div class="column"></div>
-                    <div class="column">
-                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                            <div class="field">
-                                <label class="label">Логин</label>
-                                <input class="input" type="text" name="username" value="<?php echo $username; ?>">
-                                <span class="help is-danger"><?php echo $username_err; ?></span>
-                            </div>
-                            <div class="field">
-                                <label class="label">Пароль</label>
-                                <input class="input" type="password" name="password">
-                                <span class="help is-danger"><?php echo $password_err; ?></span>
-                            </div>
-                            <div class="field">
-                                <input class="button is-link" type="submit" value="Войти">
-                            </div>
-                            <p>Нет аккаунта? <a href="register.php">Зарагестрироваться</a></p>
-                        </form>
-                    </div>
-                    <div class="column"></div>
-                </div>
-            </div>
-        </section>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <div class="field">
+        <label class="label">Логин</label>
+        <input class="input" type="text" name="username" value="<?php echo $username; ?>">
+        <span class="help is-danger"><?php echo $username_err; ?></span>
     </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-    <script src="js/main.js"></script>
-</body>
-
-</html>
+    <div class="field">
+        <label class="label">Пароль</label>
+        <input class="input" type="password" name="password">
+        <span class="help is-danger"><?php echo $password_err; ?></span>
+    </div>
+    <div class="field">
+        <input class="button is-link" type="submit" value="Войти">
+    </div>
+    <p>Нет аккаунта? <a href="register.php">Зарагестрироваться</a></p>
